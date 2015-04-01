7f8e13a19e19c341fa73a14d68202632080c541f
jagan <maddela564@gmail.com>
2015-04-01 10:10:53 -0500

    new program

--- sum.c
+++ sum.c
@@ -4,6 +4,6 @@ main()
  int i,value=10,sum=0;
  for(i=0;i<=value;i++)
  sum +=i;
- printf("the sum of a numbers of A and B is %d \n", &sum);
+ printf("the sum of a numbers  A and B is %d \n", &sum);
 }
 
